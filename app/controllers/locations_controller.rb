0cb4c40eac691e1d96846696ab85aa6f40cd368b
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-11-26 17:23:27 -0800

    add basic routes to controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,5 +5,29 @@ class LocationsController < ApplicationController
     json_response(@locations)
   end
 
+  def show
+    @location = Location.find(params[:id])
+    json_response(@location)
+  end
+
+  def create
+    @location = Location.create(location_params)
+    json_response(@location)
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+  end
+
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+  end
+
+  private
+  def location_params
+    params.permit(:location_name, :address ,:zip, :category)
+  end
 
 end
