8a2cbaba9126819570cfe9b51831c01d278234a1
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-11-26 21:53:44 -0800

    create create API key route

--- app/controllers/api_keys_controller.rb
+++ app/controllers/api_keys_controller.rb
@@ -8,9 +8,20 @@ class ApiKeysController < ApplicationController
   prepend_before_action :authenticate_with_api_key, only: [:destroy] 
   
   def index
+    render json: current_bearer.api_keys
   end
  
   def create
+    authenticate_with_http_basic do |user_name, password| 
+      user = User.find_by user_name: user_name 
+ 
+      if user&.authenticate(password) 
+        api_key = user.api_keys.create! token: SecureRandom.hex 
+ 
+        render json: api_key, status: :created and return 
+      end
+    end
+    render status: :unauthorized 
   end
  
   def destroy
