92174eca64f176efcdd9a4d8befa5d51706d4cfe
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-11-26 17:35:34 -0800

    add validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,7 @@
 class Location < ApplicationRecord
+  validates :location_name, presence: true
+  validates :address, presence: true
+  validates :zip, presence: true
+  validates :category, presence: true
+
 end
