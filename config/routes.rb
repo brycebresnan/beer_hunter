42ec7ebfe4e1831f83db7509d878282f3f8328aa
Bryce Bresnan <Bryce.Bresnan@gmail.com>
2022-11-29 23:07:41 -0800

    create custom route for barcrawl

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
   post '/api-keys', to: 'api_keys#create'
   delete '/api-keys', to: 'api_keys#destroy'
   get '/api-keys', to: 'api_keys#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get '/locations/barcrawl', to: 'locations#crawl'
+  get '/barcrawl', to: 'locations#barcrawl'
   
   resources :locations
 
